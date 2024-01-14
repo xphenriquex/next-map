05fa7e06b4d2fa1d1b92a45b76d6ef7dfec68b48
Paulo Henrique <p.henriquerm@hotmail.com>
2024-01-14 13:28:52 -0300

    Refactoring the project structure

--- app/RootComponents/header.js
+++ app/RootComponents/header.js
@@ -1,15 +1,11 @@
-"use client";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 export default function Header() {
-  const router = useRouter();
-
   return (
     <nav className='flex justify-around items-center bg-black h-14 text-white'>
-    <Link href='/'>Home</Link>
-    <button onClick={() => router.push("/about")}>About</button>
-    <button onClick={() => router.push("/contact")}>Contact</button>
-  </nav>
+      <Link href='/'>Home</Link>
+      <Link href='/request'>Trip Request</Link>
+      <Link href='/login'>Login</Link>
+    </nav>
   )
   }
---
+++ app/login/page.js
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>This is the login page. Coming soon.</h1>
+    </div>
+  )
+}
